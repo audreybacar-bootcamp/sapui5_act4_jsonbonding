436b2ab8f861d8b16b129c231363dd140848d32e
Audrey Shen Laurence Bacar <audrey.shen.l.dauzon@accenture.com>
2026-05-11 18:11:57 +0000

    working aggregate binding update

--- webapp/controller/JSONBinding.controller.js
+++ webapp/controller/JSONBinding.controller.js
@@ -1,10 +1,35 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], (Controller) => {
+], function (Controller) {
     "use strict";
 
     return Controller.extend("jsonbinding.controller.JSONBinding", {
-        onInit() {
+
+        onInit: function () {
+        },
+
+        onSelectionChange: function (oEvent) {
+
+            var oSelectedItem = oEvent.getParameter("listItem");
+
+            if (!oSelectedItem) {
+                return;
+            }
+
+            var oContext = oSelectedItem.getBindingContext("ProductsModel");
+
+            if (!oContext) {
+                return;
+            }
+
+            var oData = oContext.getObject();
+
+            console.log("Selected Product:", oData);
+
+            // OPTIONAL: directly bind selected context to panel 3
+            this.getView().byId("panel3").setBindingContext(oContext, "ProductsModel");
+
         }
+
     });
 });
